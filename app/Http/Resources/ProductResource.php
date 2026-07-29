ca926280552e941380beeb9754440ae8aa1cd490
Ronald Gutierrez Flores <deux.flores@gmail.com>
2026-07-29 14:45:17 -0400

    feat:the image was added

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -19,6 +19,7 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'price' => (float) $this->price,
             'stock' => $this->stock,
+            'image' => $this->image,
         ];
     }
 }
